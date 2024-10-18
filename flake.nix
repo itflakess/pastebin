dc1ce2f80af063ba16eba367401d37a037ac7777
example <example@example.com>
2024-10-18 23:24:13 +0800

    PUSH

--- flake.nix
+++ flake.nix
@@ -1,62 +1,24 @@
 {
-  description = "Wastebin is a pastebin";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.naersk.url = "github:nix-community/naersk";
 
-  # Nixpkgs / NixOS version to use.
-  # inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
-
-  outputs = { self, nixpkgs }:
-    let
-
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
-
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-    in
-    {
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          wastebin = with pkgs; rustPlatform.buildRustPackage rec {
-            pname = "wastebin";
-            version = "2.5.0";
-
-            src = ./.;
-
-            cargoHash = "sha256-/3nIvDueiU6WelTlgzNYWGhToDUtf3BfUCbWkJhWAaw=";
-
-            nativeBuildInputs = [ pkg-config ];
-
-            buildInputs = [ sqlite zstd ];
-
-            env.ZSTD_SYS_USE_PKG_CONFIG = true;
-
-            meta = with lib; {
-              description = "Wastebin is a pastebin";
-              homepage = "https://github.com/matze/wastebin";
-              changelog = "https://github.com/matze/wastebin/blob/${src.rev}/CHANGELOG.md";
-              license = licenses.mit;
-              maintainers = with maintainers; [ pinpox ];
-              mainProgram = "wastebin";
-            };
-          };
-        });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.wastebin);
+  outputs = {
+    self, nixpkgs, flake-utils, rust-overlay, naersk
+  }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ (import rust-overlay) ];
     };
+    rust = pkgs.rust-bin.nightly.latest.default;
+    naersk' = pkgs.callPackage naersk {
+      cargo = rust;
+      rustc = rust;
+    };
+  in {
+    packages.default = naersk'.buildPackage { src = ./.; };
+    devShells.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [ rust ];
+    };
+  });
 }
 
